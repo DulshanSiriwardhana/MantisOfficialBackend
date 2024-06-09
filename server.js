9fcaca3e3b6fc0549e93f57f32cafa66580041e1
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-09 12:01:30 +0530

    Monada mnda npm model godk dAmma , eth error enawa

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const mongodb = require('mongodb');
+const { ClientEncryption } = require('mongodb-client-encryption');
+const { MongoClient } = require('mongodb');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
--- webpack.dev.js
+++ webpack.dev.js
@@ -19,7 +19,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "build-loader"
+                loader: "babel-loader"
             }
         ]
     }
